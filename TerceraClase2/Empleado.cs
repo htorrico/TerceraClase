0306e88d87b057b531bbff5cbbf1e0fd3265e6c6
Hugo Felipe Torrico Marquez <htorrico@tecsup.edu.pe>
2021-09-04 09:07:28 -0500

    Add project files.

---
+++ TerceraClase2/Empleado.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerceraClase2
+{
+  public  class Empleado
+    {
+       
+        public string CodigoEmpleado { get; set; }
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
+        public string PrimerNombre { get; set; }
+        public string SegundoNombre { get; set; }
+        public decimal SueldoBruto { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public DateTime FechaIngreso { get; set; }
+
+        public string RetornarNombreCompleto()
+        {
+
+            //PrimerApellido + Segundo Apellido + Primer Nombre + Segundo Nombre
+            string resultado = "";
+
+            //resultado = PrimerApellido + " " + SegundoApellido + " " + PrimerApellido + " " + SegundoNombre;
+            resultado = string.Concat(PrimerApellido, " ", SegundoApellido, " ", PrimerNombre, " ", SegundoNombre);
+
+            return resultado;
+        }
+
+        public decimal RetornarAporte()
+        {
+            decimal resultado = 0.0m;
+            resultado = SueldoBruto * 0.13m;
+            return resultado;
+        }
+
+
+        public decimal RetornarSueldoNeto()
+        {
+            //SueldoNeto = SueldoBruto - AporteAFP
+            decimal resultado = 0.0m;
+            resultado = SueldoBruto - RetornarAporte();
+            return resultado;
+        }
+
+    }
+}
---
+++ TerceraClase2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerceraClase2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Empleado empleado = new Empleado();
+
+            //Obtener Datos
+            Console.WriteLine("Ingrese Primer Apellido");
+            empleado.PrimerApellido = Console.ReadLine();
+
+            Console.WriteLine("Ingrese Segundo Apellido");
+            empleado.SegundoApellido = Console.ReadLine();
+
+            Console.WriteLine("Ingrese Primer Nombre");
+            empleado.PrimerNombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese Segundo Nombre");
+            empleado.SegundoNombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese Segundo Bruto");
+            empleado.SueldoBruto =Convert.ToDecimal( Console.ReadLine());
+
+            //Calcular Datos
+            string NombreCompleto = empleado.RetornarNombreCompleto();
+            decimal SueldoNeto = empleado.RetornarSueldoNeto();
+
+
+            //Mostrar Datos
+            Console.WriteLine("El nombre completo es:"+ NombreCompleto );
+            Console.WriteLine("El sueldo neto es:" + SueldoNeto);
+
+
+            Console.Read();
+        }
+    }
+}
